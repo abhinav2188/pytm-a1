bc6ca2d3989171c844ac21e391823bb0595c3bfb
abhinav2188 <abhinav.mca19.du@gmail.com>
2022-01-05 18:34:19 +0530

    initial commit

---
+++ src/test/java/com/paytm/assignment1/Assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paytm.assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
